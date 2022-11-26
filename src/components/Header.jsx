535b1716548e19480c6d3fa4e32646287afc6e34
Vitalmy <vitmylock@yandex.ru>
2022-11-26 19:13:58 +0300

    update module.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./Header.css";
+import classes from "./Header.module.css";
 const Header = () => {
   return (
-    <header className="header">
+    <header className={classes.header}>
       <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" />
     </header>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import "./Profile.css";
+import classes from "./Profile.module.css";
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={classes.content}>
       <div>
         <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" />
       </div>
       <div> ava desc</div>
-      <div>
+      <div className={classes.posts}>
         {" "}
         my posts
         <div> new post </div>
-      </div>
-      <div> post 1</div>
-      <div> post 2</div>
+      <div className={classes.item}> post 1</div>
+      <div className={classes.item}> post 2</div>
+    </div>
     </div>
   );
 };
