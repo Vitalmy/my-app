30c9fd866e2b59a4b73b5c3eb7b62daacff2ad3a
Vitalmy <vitmylock@yandex.ru>
2022-11-27 18:12:23 +0300

    routmap created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -17,7 +19,9 @@ const App = () => {
       <Routes>
       <Route path='/dialogs' element = {<Dialogs />}/>
       <Route path='/profile' element = {<Profile />}/>
+      <Route path='/news' element = {<News />}/>
       <Route path='/music' element = {<Music />}/>
+      <Route path='/settings' element = {<Settings />}/>
       </Routes>
      </div> 
     </div>
