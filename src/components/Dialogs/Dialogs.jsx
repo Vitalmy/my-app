9c2b391b3a0a087e62424b5b072fb0906e08bd71
Vitalmy <vitmylock@yandex.ru>
2022-11-29 00:47:33 +0300

    add dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,34 @@ import React from "react";
 import s from "./Dialogs.module.css"
 const Dialogs = () => {
   return (
-  <div>
-    Dialogs
+  <div className={s.dialogs}>
+<div className={s.dialogsItems}>
+  <div className={`${s.dialog} ${s.active}`}>
+    Dimych
+  </div>
+  <div className={s.dialog}>
+Andrey
+  </div>
+  <div className={s.dialog}>
+Sveta
+  </div>
+  <div className={s.dialog}>
+Sasha
+  </div>
+  <div className={s.dialog}>
+Victor
+  </div>
+  <div className={s.dialog}>
+Valera
+  </div>
+</div>
+<div className={s.messages}>
+<div className={s.message}>Hi</div>
+<div className={s.message}>How is your it-cam</div>
+<div className={s.message}>yo</div>
+</div>
     </div>
+    
   );
 };
 export default Dialogs;
