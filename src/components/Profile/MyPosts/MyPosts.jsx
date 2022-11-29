1295c0775303ffafb7a3d77ecec17bc62cdf251a
Vitalmy <vitmylock@yandex.ru>
2022-11-29 03:24:23 +0300

    was decorated

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,12 +3,15 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post"
 const MyPosts = () => {
   return (
-      <div>
-        my posts
+      <div className={s.postBlock}>
+       <h3>My posts</h3>
         <div>
+          <div>
           <textarea> </textarea>
+          </div>
+          <div>
           <button>Add Post</button>
-
+          </div>
            </div>
            <div  className={s.posts}>
            <Post message = "Hi, how are you?" likes = "12" />
