0bf8d7dd4ccf7d80513ad5de356137fe98086e50
Vitalmy <vitmylock@yandex.ru>
2022-12-07 20:49:03 +0300

    added massive - map

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,14 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post"
 const MyPosts = () => {
 
-  let postsData = [
+  let posts = [
     {id: 1, message: 'Hi, how are you mm?', likesCount:12},
     {id: 2, message: 'It\'s my first post', likesCount:11},
   ]
+
+let postsElements =
+posts.map ( p => <Post message={p.message} likesCount={p.likesCount} />);
+
   return (
       <div className={s.postBlock}>
        <h3>My posts</h3>
@@ -19,8 +23,7 @@ const MyPosts = () => {
           </div>
            </div>
            <div  className={s.posts}>
-           <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
-           <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
+            {postsElements}
            </div>
     </div>
   );
