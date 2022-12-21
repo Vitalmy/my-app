077e5fe30639143d7fb8b9775cd6170742270b07
Vitalmy <vitmylock@yandex.ru>
2022-12-21 23:15:45 +0300

    side effect added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,56 @@
+import axios from "axios";
 import React from "react";
 import styles from "./users.module.css";
+import userPhoto from "../../assets/images/avatar.jpg";
+
+// [
+//   {
+//     id: 1,
+//     photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
+//     followed: false,
+//     fullName: "Dmitry",
+//     status: "i am a boss",
+//     location: { city: "Minsk", country: "Belarus" },
+//   },
+//   {
+//     id: 2,
+//     photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
+//     followed: true,
+//     fullName: "Sasha",
+//     status: "i am a boss too",
+//     location: { city: "Moscow", country: "Russia" },
+//   },
+//   {
+//     id: 3,
+//     photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
+//     followed: false,
+//     fullName: "Dmitry",
+//     status: "i am a boss too",
+//     location: { city: "Pinsk", country: "Belarus" },
+//   },
+// ]
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
-        followed: false,
-        fullName: "Dmitry",
-        status: "i am a boss",
-        location: { city: "Minsk", country: "Belarus" },
-      },
-      {
-        id: 2,
-        photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
-        followed: true,
-        fullName: "Sasha",
-        status: "i am a boss too",
-        location: { city: "Moscow", country: "Russia" },
-      },
-      {
-        id: 3,
-        photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
-        followed: false,
-        fullName: "Dmitry",
-        status: "i am a boss too",
-        location: { city: "Pinsk", country: "Belarus" },
-      },
-    ]);
-  }
-
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} alt="peedor" className={styles.userPhoto} />
+              <img
+                src={u.photos.small != null ? u.photos.small : userPhoto}
+                className={styles.userPhoto}
+              />
             </div>
             <div>
               {u.followed ? (
@@ -61,12 +74,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
