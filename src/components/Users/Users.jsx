fcc026e5e3843a7eda63ab8dd4df8c6ada32e162
Vitalmy <vitmylock@yandex.ru>
2022-12-22 17:31:22 +0300

    22.12

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,89 +3,63 @@ import React from "react";
 import styles from "./users.module.css";
 import userPhoto from "../../assets/images/avatar.jpg";
 
-// [
-//   {
-//     id: 1,
-//     photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
-//     followed: false,
-//     fullName: "Dmitry",
-//     status: "i am a boss",
-//     location: { city: "Minsk", country: "Belarus" },
-//   },
-//   {
-//     id: 2,
-//     photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
-//     followed: true,
-//     fullName: "Sasha",
-//     status: "i am a boss too",
-//     location: { city: "Moscow", country: "Russia" },
-//   },
-//   {
-//     id: 3,
-//     photoUrl: "https://avatar-collection.ru/images/avatar-687.jpg",
-//     followed: false,
-//     fullName: "Dmitry",
-//     status: "i am a boss too",
-//     location: { city: "Pinsk", country: "Belarus" },
-//   },
-// ]
-
-let Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          props.setUsers(response.data.items);
-        });
-    }
-  };
-  return (
-    <div>
-      <button onClick={getUsers}>Get Users</button>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                src={u.photos.small != null ? u.photos.small : userPhoto}
-                className={styles.userPhoto}
-              />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
+class Users extends React.Component {
+  constructor(props) {
+    alert("new");
+    super(props);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id}>
             <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
+              <div>
+                <img
+                  src={u.photos.small != null ? u.photos.small : userPhoto}
+                  className={styles.userPhoto}
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
             </span>
             <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
