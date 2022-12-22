84ea940e2aba6b05566482ab787101fef3f0bd3a
Vitalmy <vitmylock@yandex.ru>
2022-12-22 17:50:01 +0300

    componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,15 +4,14 @@ import styles from "./users.module.css";
 import userPhoto from "../../assets/images/avatar.jpg";
 
 class Users extends React.Component {
-  constructor(props) {
-    alert("new");
-    super(props);
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
         this.props.setUsers(response.data.items);
       });
   }
+
   render() {
     return (
       <div>
