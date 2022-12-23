c453e2295a065c824dd984d0b121194bcb5a2178
Vitalmy <vitmylock@yandex.ru>
2022-12-23 23:59:46 +0300

    profile page added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
@@ -19,7 +19,7 @@ const App = (props) => {
         <div class="app-wrapper-content">
           <Routes>
             <Route path="/dialogs" element={<DialogsContainer />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/*" element={<ProfileContainer />} />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} />
