b5243f035bf65bb93f3f9da3b7892d8a07e7718d
Vitalmy <vitmylock@yandex.ru>
2022-12-26 00:19:47 +0300

    DAL added

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "4e22cfb5-67d8-46f4-bf12-065c2f918c77",
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
