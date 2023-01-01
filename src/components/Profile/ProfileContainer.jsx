6635e54d2cf1c2ae6ccd96d0468ba6e15c0252c2
Vitalmy <vitmylock@yandex.ru>
2023-01-01 19:56:35 +0300

    http put

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,7 +3,11 @@ import s from "./Profile.module.css";
 import Profile from "./Profile";
 import { connect } from "react-redux";
 import axios from "axios";
-import { getUserProfile } from "../../redux/profile-reducer";
+import {
+  getUserProfile,
+  getStatus,
+  updateStatus,
+} from "../../redux/profile-reducer";
 import { Navigate, useParams } from "react-router-dom";
 import { usersAPI } from "../../api/api";
 import { withAuthRedirect } from "../../hoc/withAuthRedirect";
@@ -20,20 +24,31 @@ class ProfileContainer extends React.Component {
   componentDidMount() {
     let userId = this.props.match.params.userId;
     if (!userId) {
-      userId = 2;
+      userId = 27236;
     }
     this.props.getUserProfile(userId);
+    this.props.getStatus(userId);
   }
 
   render() {
-    return <Profile {...this.props} profile={this.props.profile} />;
+    return (
+      <Profile
+        {...this.props}
+        profile={this.props.profile}
+        status={this.props.status}
+        updateStatus={this.props.updateStatus}
+      />
+    );
   }
 }
 
-let MapStateToProps = (state) => ({ profile: state.profilePage.profile });
+let MapStateToProps = (state) => ({
+  profile: state.profilePage.profile,
+  status: state.profilePage.status,
+});
 
 export default compose(
-  connect(MapStateToProps, { getUserProfile }),
+  connect(MapStateToProps, { getUserProfile, getStatus, updateStatus }),
   withRouter
   // withAuthRedirect
 )(ProfileContainer);
